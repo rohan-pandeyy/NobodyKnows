c30d72c98761c7442fcca98fe0289eec8f363439
ROHAN PANDEY <95585299+rohan-pandeyy@users.noreply.github.com>
2025-02-04 19:31:38 +0530

    Added Main Menu

---
+++ Assets/UI/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
